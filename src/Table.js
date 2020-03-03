3d28b8cabf3e9109a689b21415a6ed8338448cac
Polina Chernomaz <polina@crosscompute.com>
2020-03-03 08:54:42 -0500

    Added searching, filtering, and sorting features

---
+++ src/Table.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import MaterialTable from 'material-table'
+
+class Table extends React.Component {
+  render() {
+    return (
+      <MaterialTable
+          title="Basic Searching, Filtering, Sorting Preview"
+          columns={[
+            { 
+              title: 'Name', 
+              field: 'name', 
+              customFilterAndSearch: (term, rowData) => term == rowData.name.length 
+            },
+            { title: 'Surname', field: 'surname' },
+            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            {
+              title: 'Birth Place',
+              field: 'birthCity',
+              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+            },
+          ]}
+          data={[
+            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+          ]}        
+          options={{
+            sorting: true,
+            filtering: true,
+          }}
+        />
+    )
+  }
+}
+
+export default Table
