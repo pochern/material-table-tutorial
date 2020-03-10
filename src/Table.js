1a2dbd364eeae630c723b4a53f57a624d87015e2
Polina Chernomaz <polina@crosscompute.com>
2020-03-10 10:59:12 -0400

    Added Selection, Grouping, Styling features
    - Added MuiTheme in constructor with palette - changes selection to orange
    - Added selectedRow in this.state to change color of row onClick
    - In options, added rowStyle for row color change to grey, selection set to true, and grouping set to true
    Added cell header styling changes to column ‘Name'

--- src/Table.js
+++ src/Table.js
@@ -1,34 +1,69 @@
 import React from 'react';
 import MaterialTable from 'material-table'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props)
+    this.theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#4caf50',
+        },
+        secondary: {
+          main: '#ff9100',
+        },
+      },
+
+    })
+    this.state = {
+      selectedRow: null
+    }
+  }
+
   render() {
     return (
-      <MaterialTable
-          title="Basic Searching, Filtering, Sorting Preview"
-          columns={[
-            { 
-              title: 'Name', 
-              field: 'name', 
-              customFilterAndSearch: (term, rowData) => term == rowData.name.length 
-            },
-            { title: 'Surname', field: 'surname' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            {
-              title: 'Birth Place',
-              field: 'birthCity',
-              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-            },
-          ]}
-          data={[
-            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-          ]}        
-          options={{
-            sorting: true,
-            filtering: true,
-          }}
-        />
+        <MuiThemeProvider theme={this.theme}>
+          <MaterialTable
+            title="Basic Selection, Grouping, Styling Preview"
+            columns={[
+              {
+                title: 'Name',
+                field: 'name',
+                cellStyle: {
+                  backgroundColor: '#039be5',
+                  color: '#FFF'
+                },
+                headerStyle: {
+                  backgroundColor: '#039be5',
+                },
+                customFilterAndSearch: (term, rowData) => term == rowData.name.length
+              },
+              { title: 'Surname', field: 'surname', },
+              { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+              {
+                title: 'Birth Place',
+                field: 'birthCity',
+                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+              },
+            ]}
+            data={[
+              { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+              { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+            ]}
+            onRowClick={((evt, selectedRow) => this.setState({ selectedRow }))}
+            options={{
+              sorting: true,
+              filtering: true,
+              grouping: true,
+              selection: true,
+              rowStyle: rowData => ({
+                  backgroundColor: (this.state.selectedRow && this.state.selectedRow.tableData.id === rowData.tableData.id) ? '#EEE' : '#FFF'
+              })
+            }}
+          />
+      </MuiThemeProvider>
     )
   }
 }
