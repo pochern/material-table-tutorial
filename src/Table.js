2139817592630080b2595629d9b4e45a22976908
Polina Chernomaz <polina@crosscompute.com>
2020-03-24 04:21:15 -0400

    Added Remote Data, Fixed Columns, and Actions

--- src/Table.js
+++ src/Table.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import MaterialTable from 'material-table'
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 
 class Table extends React.Component {
   constructor(props) {
@@ -18,51 +18,84 @@ class Table extends React.Component {
 
     })
     this.state = {
-      selectedRow: null
+      selectedRow: null,
     }
   }
-
   render() {
     return (
-        <MuiThemeProvider theme={this.theme}>
-          <MaterialTable
-            title="Basic Selection, Grouping, Styling Preview"
-            columns={[
-              {
-                title: 'Name',
-                field: 'name',
-                cellStyle: {
-                  backgroundColor: '#039be5',
-                  color: '#FFF'
-                },
-                headerStyle: {
-                  backgroundColor: '#039be5',
-                },
-                customFilterAndSearch: (term, rowData) => term == rowData.name.length
+      <MuiThemeProvider theme={this.theme}>
+        <MaterialTable
+          title='Remote Data, Fixed Columns, Actions'
+          columns={[
+            {
+              title: 'Id',
+              field: 'id',
+              cellStyle: {
+                backgroundColor: '#039be5',
+                color: '#FFF'
               },
-              { title: 'Surname', field: 'surname', },
-              { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-              {
-                title: 'Birth Place',
-                field: 'birthCity',
-                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+              headerStyle: {
+                backgroundColor: '#039be5',
               },
-            ]}
-            data={[
-              { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-              { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-            ]}
-            onRowClick={((evt, selectedRow) => this.setState({ selectedRow }))}
-            options={{
-              sorting: true,
-              filtering: true,
-              grouping: true,
-              selection: true,
-              rowStyle: rowData => ({
-                  backgroundColor: (this.state.selectedRow && this.state.selectedRow.tableData.id === rowData.tableData.id) ? '#EEE' : '#FFF'
+              customFilterAndSearch: (term, rowData) => term == rowData.name.length,
+              width: 150,
+            },
+            { title: 'First Name', field: 'first_name', width: 150, },
+            { title: 'Last Name', field: 'last_name', width: 150},
+
+            { title: 'First Name', field: 'first_name', width: 150, },
+            { title: 'Last Name', field: 'last_name', width: 150},
+
+            { title: 'First Name', field: 'first_name', width: 150, },
+            { title: 'Last Name', field: 'last_name', width: 150},
+
+            { title: 'First Name', field: 'first_name', width: 150},
+            { title: 'Last Name', field: 'last_name', width: 150},
+
+            { title: 'First Name', field: 'first_name', width: 150},
+            { title: 'Last Name', field: 'last_name', width: 150},
+          ]}
+          onRowClick={((evt, selectedRow) => this.setState({ selectedRow }))}
+          // remote data
+          data={query =>
+            new Promise((resolve, reject) => {
+              fetch('data')
+              .then(response => response.json())
+              .then(result => {
+                resolve({
+                  data: result.data,
+                  page: result.page - 1,
+                  totalCount: result.total,
+                })
               })
-            }}
-          />
+            })
+          }
+          actions={[
+            {
+              icon: 'save',
+              tooltip: 'Save User',
+              onClick: (event, rowData) => alert('You saved ' + rowData[0].first_name)
+            },
+            {
+              icon: 'delete',
+              tooltip: 'Delete User',
+              onClick: (event, rowData) => window.confirm("You want to delete " + rowData[0].first_name)
+            }
+          ]}
+          options={{
+            sorting: true,
+            filtering: true,
+            selection: true,
+            grouping: true,
+            fixedColumns: {
+              left: 2,
+              right: 0
+            },
+            rowStyle: rowData => ({
+                backgroundColor: (this.state.selectedRow && this.state.selectedRow.tableData.id === rowData.tableData.id) ? '#EEE' : '#FFF'
+            })
+          }}
+        />
       </MuiThemeProvider>
     )
   }
